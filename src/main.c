3b09774020b47a9af6c3f21fcea5cbedff971752
zChar <105929248+zcharo99@users.noreply.github.com>
2025-03-08 21:32:19 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 
+void firesh_loop(void)
+{
+  char *line;
+  char **args;
+  int status;
+
+  do {
+    printf("> ");
+    line = firesh_read_line();
+    args = firesh_split_line(line);
+    status = firesh_execute(args);
+
+    free(line);
+    free(args);
+  } while (status);
+}
+
+int main(int argc, char **argv) {
+    firesh_loop();
+    return 0;
+}
